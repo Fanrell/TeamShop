cb58d44bc9baee50d1872571af5462a71b046b28
fanrell <cieslik.kamil@outlook.com>
2020-11-02 10:11:16 +0100

    first commit

---
+++ TeamShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamShop.Models;
+
+namespace TeamShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=127.0.0.1;port=3306;user=root;database=TeamShopDb")
+                .UseLoggerFactory(LoggerFactory.Create(b => b.AddConsole().AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
