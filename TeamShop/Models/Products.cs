0532ae0a99dc688524dba811d5f0f9f527c1a403
Yankgit <jakub.kosek97@wp.pl>
2020-11-16 18:17:16 +0100

    Panel edition
    Panel edition

---
+++ TeamShop/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamShop.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public float Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public string Tags { get; set; }
+        [Required]
+        public string Types { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Attach1 { get; set; }
+    }
+}
